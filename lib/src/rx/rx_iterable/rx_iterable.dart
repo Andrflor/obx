38ae4e0fb5d8b57129195a737d75d0f7fa1da80c
andrflor <andrieu.florian@mail.com>
2022-08-29 17:20:04 +0200

    Added iterable functions

--- lib/src/rx/rx_iterable/rx_iterable.dart
+++ lib/src/rx/rx_iterable/rx_iterable.dart
@@ -6,6 +6,7 @@ extension RxIterableExt<T extends Iterable<E>, E> on Rx<T> {
       pipe((e) => e.map((event) => event.cast<S>()), init: (e) => e.cast<S>());
 
   Iterable<S> cast<S>() => value.cast<S>();
+
   int get length => value.length;
   bool get isEmpty => value.isEmpty;
   bool get isNotEmpty => value.isNotEmpty;
@@ -32,17 +33,74 @@ extension RxIterableExt<T extends Iterable<E>, E> on Rx<T> {
   Iterable<S> expand<S>(Iterable<S> Function(E element) toElements) =>
       value.expand<S>(toElements);
 
-  Iterable<E> reduce() => value.reduce((value, element) => null);
-  Iterable<E> fold() =>
-      value.fold(initialValue, (previousValue, element) => null);
-  Iterable<E> every() => value.every((element) => false);
-  Iterable<E> join() => value.join();
-  Iterable<E> any() => value.any((element) => false);
-  Iterable<E> take() => value.take(count);
-  Iterable<E> takeWhile() => value.takeWhile((value) => false);
-  Iterable<E> skip() => value.skip(count);
-  Iterable<E> skipWhile() => value.skipWhile((value) => false);
+  E reduce(E Function(E value, E element) combine) => value.reduce(combine);
+  S fold<S>(S initialValue, S Function(S previousValue, E element) combine) =>
+      value.fold<S>(initialValue, combine);
+
+  String join([String separator = ""]) => value.join(separator);
+  bool every(bool Function(E element) test) => value.every(test);
+  bool any(bool Function(E element) test) => value.any(test);
+
+  Iterable<E> take(int count) => value.take(count);
+  Iterable<E> takeWhile(bool Function(E element) test) => value.takeWhile(test);
+
+  Iterable<E> skip(int count) => value.skip(count);
+  Iterable<E> skipWhile(bool Function(E element) test) => value.skipWhile(test);
 
   Set<E> toSet() => value.toSet();
   List<E> toList({bool growable = true}) => value.toList(growable: growable);
+}
+
+extension RxnIterableExt<T extends Iterable<E>?, E> on Rx<T> {
+  Iterator<E>? get iterator => value?.iterator;
+  Rx<Iterable<S>?> rxCast<S>() =>
+      pipe((e) => e.map((event) => event?.cast<S>()),
+          init: (e) => e?.cast<S>());
+
+  Iterable<S>? cast<S>() => value?.cast<S>();
+
+  int? get length => value?.length;
+  bool? get isEmpty => value?.isEmpty;
+  bool? get isNotEmpty => value?.isNotEmpty;
+
+  E? get first => value?.first;
+  E? get last => value?.last;
+  E? get single => value?.single;
+
+  bool? contains(Object? element) => value?.contains(element);
+  void forEach(void Function(E element) action) => value?.forEach(action);
+
+  Iterable<E>? where(bool Function(E e) test) => value?.where(test);
+
+  E? singleWhere(bool Function(E element) test, {E Function()? orElse}) =>
+      value?.singleWhere(test, orElse: orElse);
+  E? firstWhere(bool Function(E element) test, {E Function()? orElse}) =>
+      value?.firstWhere(test, orElse: orElse);
+  E? lastWhere(bool Function(E element) test, {E Function()? orElse}) =>
+      value?.lastWhere(test, orElse: orElse);
+  E? elementAt(int index) => value?.elementAt(index);
+
+  Iterable<S>? map<S>(S Function(E e) toElement) => value?.map<S>(toElement);
+  Iterable<S>? whereType<S>() => value?.whereType<S>();
+  Iterable<S>? expand<S>(Iterable<S> Function(E element) toElements) =>
+      value?.expand<S>(toElements);
+
+  E? reduce(E Function(E value, E element) combine) => value?.reduce(combine);
+  S fold<S>(S initialvalue, S Function(S previousvalue, E element) combine) =>
+      value?.fold<S>(initialvalue, combine) ?? initialvalue;
+
+  String? join([String separator = ""]) => value?.join(separator);
+  bool? every(bool Function(E element) test) => value?.every(test);
+  bool? any(bool Function(E element) test) => value?.any(test);
+
+  Iterable<E>? take(int count) => value?.take(count);
+  Iterable<E>? takeWhile(bool Function(E element) test) =>
+      value?.takeWhile(test);
+
+  Iterable<E>? skip(int count) => value?.skip(count);
+  Iterable<E>? skipWhile(bool Function(E element) test) =>
+      value?.skipWhile(test);
+
+  Set<E>? toSet() => value?.toSet();
+  List<E>? toList({bool growable = true}) => value?.toList(growable: growable);
 }
